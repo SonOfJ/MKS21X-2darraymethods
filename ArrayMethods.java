8cf8a5d791c195d85caeb7f66213534343580c41
Jason Zheng <zzheng3@stuy.edu>
2018-11-05 22:27:52 -0500

    added method to deal with rowSum cases where x is past the last row

--- ArrayMethods.java
+++ ArrayMethods.java
@@ -1,5 +1,8 @@
 public class ArrayMethods {
   public static int rowSum(int[][] ary, int x) {
+    if (x >= ary.length) {
+      return 0;
+    }
     int sum = 0;
     for(int i = 0; i < ary[x].length; i = i + 1) {
       sum = sum + ary[x][i];
